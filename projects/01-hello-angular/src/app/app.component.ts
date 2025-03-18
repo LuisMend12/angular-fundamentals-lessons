8b6f8472bae1d950a75686620b4c14dd85f874ab
LuisM23 <128239393+LuisMend12@users.noreply.github.com>
2025-03-18 13:03:25 -0400

    new

--- projects/01-hello-angular/src/app/app.component.ts
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -9,6 +9,10 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>If you are reading this...</h1>
     <p>Things have worked out well! 🎉</p>
+    <ol>
+      <li>ETA - NewJeans</li>
+      
+    </ol>
   `,
   styles: ``,
 })
