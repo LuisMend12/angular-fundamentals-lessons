9cfb4d602d3153b9c63fd2c09f9c40f7d30387f7
LuisM23 <128239393+LuisMend12@users.noreply.github.com>
2025-03-31 19:03:23 -0400

    new

--- projects/11-dependency-injection/src/app/app.component.ts
+++ projects/11-dependency-injection/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { data, User } from './data';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { DetailsComponent } from "../../../08-routing-recap/src/app/details/details.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: ` <h1>User Listing</h1> `,
+  imports: [UserInfoComponent],
+  template: ` 
+  <h1>User Listing</h1> 
+  @for(user of userData; track user.id) {
+    <app-user-info [user] = "user" />
+  }
+  `,
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  userService = inject(UserService);
+  userData: User[] = [];
+
+
+  async ngOnInit(): Promise<void> {
+    const data = await this.userService.getUserData()
+    this.userData = data;
+  }
+  // async constructor
+  //lifecycle of the component happens onint before the constructor, safes way to make api calls that are not fsat
+
+  // constructor() {
+  //   this.userService.getUserData().then((data) =>{
+  //     this.userData = data;
+  //   });
+  // }
 }
